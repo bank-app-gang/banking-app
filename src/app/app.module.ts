e655517fe156e120bcde7243f34f088031236718
osmankabdulla <osmankabdulla@users.noreply.github.com>
2019-07-12 16:08:43 -0400

    fixed2.0
    fixed app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import{FormsModule,ReactiveFormsModule} from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
 import { UserComponent } from './user/user.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MyAccountComponent } from './my-account/my-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInFormComponent,
-    UserComponent
+    UserComponent,
+    MyAccountComponent,
+   
   ],
   imports: [
     BrowserModule,

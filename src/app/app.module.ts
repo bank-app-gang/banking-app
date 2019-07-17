4280d743e8e6cc2e71763734535be5d0edb285cb
osmankabdulla <osmankabdulla@users.noreply.github.com>
2019-07-17 14:03:48 -0400

    register and my account page working
    register/my account styled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material';
 
 
 @NgModule({
@@ -38,6 +39,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [HttpClientModule,AuthenticateService],
   bootstrap: [AppComponent]
